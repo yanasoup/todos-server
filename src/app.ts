28011b5084b4c35a0814fa5ee754f1933716aae2
yanasoup <yana.soup@gmail.com>
2025-04-17 10:33:46 +0700

    add vercel.json

--- src/app.ts
+++ src/app.ts
@@ -4,89 +4,101 @@ import path from 'path';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import { todosRouter } from './routes/todos';
-
-type SwaggerRequest = {
-  headers: Record<string, string>;
-  method?: string;
-  url?: string;
-};
-
-const swaggerOptions = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Web Programming Hack - Todo API',
-      version: '1.0.0',
-      description: 'API documentation for managing todos',
-    },
-    tags: [
-      {
-        name: 'Todos',
-        description: 'Operations related to todo items',
-      },
-    ],
-    components: {
-      schemas: {
-        Todo: {
-          type: 'object',
-          properties: {
-            id: { type: 'string', description: 'The unique ID of the todo' },
-            title: { type: 'string', description: 'The title of the todo' },
-            completed: {
-              type: 'boolean',
-              description: 'Whether the todo is completed',
-            },
-          },
-          required: ['id', 'title', 'completed'],
-        },
-        NewTodo: {
-          type: 'object',
-          properties: {
-            title: { type: 'string', description: 'The title of the todo' },
-            completed: {
-              type: 'boolean',
-              description: 'Whether the todo is completed',
-            },
-          },
-          required: ['title', 'completed'],
-        },
-      },
-    },
-  },
-  apis: ['./src/routes/todos/*.ts'],
-};
-
-const swaggerDocs = swaggerJsdoc(swaggerOptions);
+import cluster from 'cluster';
 
 export const app = express();
 
-app.use(cors());
-app.use(express.json());
+if (cluster.isPrimary) {
+  cluster.fork();
 
-app.use(
-  '/swagger-ui.css',
-  express.static(path.join(__dirname, 'css/swagger-ui.css'))
-);
+  cluster.on('exit', function () {
+    cluster.fork();
+  });
+}
 
-app.use(
-  '/api-docs',
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerDocs, {
-    customCssUrl: '/swagger-ui.css',
-    customSiteTitle: 'Todo API Documentation',
-    swaggerOptions: {
-      requestInterceptor: (req: SwaggerRequest) => {
-        console.log('Intercepting request:', req);
-        req.headers['api-key'] = '0ICVyrNhPL56Oss58qv-_y42PhSQvYcPm6Vz26j4bNw';
-        return req;
+if (cluster.isWorker) {
+  type SwaggerRequest = {
+    headers: Record<string, string>;
+    method?: string;
+    url?: string;
+  };
+
+  const swaggerOptions = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Todo API',
+        version: '1.0.0',
+        description: 'API documentation for managing todos',
+      },
+      tags: [
+        {
+          name: 'Todos',
+          description: 'Operations related to todo items',
+        },
+      ],
+      components: {
+        schemas: {
+          Todo: {
+            type: 'object',
+            properties: {
+              id: { type: 'string', description: 'The unique ID of the todo' },
+              title: { type: 'string', description: 'The title of the todo' },
+              completed: {
+                type: 'boolean',
+                description: 'Whether the todo is completed',
+              },
+            },
+            required: ['id', 'title', 'completed'],
+          },
+          NewTodo: {
+            type: 'object',
+            properties: {
+              title: { type: 'string', description: 'The title of the todo' },
+              completed: {
+                type: 'boolean',
+                description: 'Whether the todo is completed',
+              },
+            },
+            required: ['title', 'completed'],
+          },
+        },
       },
     },
-  })
-);
+    apis: ['./src/routes/todos/*.ts'],
+  };
 
-app.use('/todos', todosRouter);
+  const swaggerDocs = swaggerJsdoc(swaggerOptions);
 
-app.listen(8080, () => {
-  console.log('Server running on port 8080');
-  console.log('Swagger docs available at http://localhost:8080/api-docs');
-});
+  app.use(cors());
+  app.use(express.json());
+
+  app.use(
+    '/swagger-ui.css',
+    express.static(path.join(__dirname, 'css/swagger-ui.css'))
+  );
+
+  app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocs, {
+      customCssUrl: '/swagger-ui.css',
+      customSiteTitle: 'Todo API Documentation',
+      swaggerOptions: {
+        requestInterceptor: (req: SwaggerRequest) => {
+          console.log('Intercepting request:', req);
+          req.headers['api-key'] =
+            '0ICVyrNhPL56Oss58qv-_y42PhSQvYcPm6Vz26j4bNw';
+          return req;
+        },
+      },
+    })
+  );
+
+  app.use('/todos', todosRouter);
+
+  app.listen(8080, () => {
+    console.log('Server running on port 8080');
+    console.log('Swagger docs available at http://localhost:8080/api-docs');
+  });
+}
